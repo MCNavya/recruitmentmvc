49f1b422b57d2be74956722d2a31be968b914cfb
socgend11@iiht.tech <socgend11@iiht.tech>
2019-09-11 11:12:41 +0530

    Add project files.

---
+++ Recruitment_Application/Controllers/RequirementController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Recruitment_Application.Controllers
+{
+    public class RequirementController : Controller
+    {
+        // GET: Requirement
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ReqInput()
+        {
+            return View();
+        }
+    }
+}
